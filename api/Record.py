5cf54789c725607abb621cab1ab7a1c92991ab93
catking562 <taewookim562@gmail.com>
2024-11-15 11:42:39 +0900

    edit Record.py

--- api/Record.py
+++ api/Record.py
@@ -1,6 +1,12 @@
-class my_class(object):
-    pass
+dictionary = {}
 
+def reset():
+    #데이터 초기화
+    return True;
 
+def input_data(time, data):
+    #데이터 넣기
+    return True;
 
-
+def getSaves():
+    return dictionary;
