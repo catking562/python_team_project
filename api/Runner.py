258a098cf8c39c8f23cfb0642b51159496cdea2c
leekhred <163300190+leekhred@users.noreply.github.com>
2024-11-17 20:23:39 +0900

    Update Runner.py
    일단은 출력시키는 것만 해놨습니다

--- api/Runner.py
+++ api/Runner.py
@@ -1,25 +1,41 @@
+import pyautogui as pg
+
+
 """
 참고사항: 받은 dictionary데이터는
 {<다음동작까지 걸리는 시간>:<다음 동작>}
 형태로 이루어져 있음
 """
+# 직접 생성
+my_dict = {'name': 'Alice', 'age': 25 , 'gender' : 'female'}
 
-isStop = False;
+
+
+isStop = False
 
 def start(dictionary):
-    isStop = False;
+    isStop = False
     #dictionary에 들어있는 행동들을 반복
     while not isStop:
-        print("반복중"); #이 줄은 삭제
-    return True;
+        for i in dictionary.values():
+            print(i) #i를 출력 검사 : 여기다가 행동 넣을 듯
+
+            pg.sleep(1)
+        return True
+
+
+start(my_dict)
 
 def repeatStart(dictionary):
     isStop = False;
     #dictionary에 들어있는 행동들을 무한반복
     while not isStop:
-        print("반복중") #이 줄은 삭제
-    return True;
+        for i in dictionary.values():
+            print(i) #i를 출력 검사 : 여기다가 행동 넣을 듯
+
+            pg.sleep(1)
+        return True;
 
 def stop():
-    isStop = True;
-    return True;
+    isStop = True
+    return True
