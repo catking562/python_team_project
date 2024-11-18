cf2db7f53e14381224538c35afdb51f073d58ea3
leekhred <163300190+leekhred@users.noreply.github.com>
2024-11-18 19:19:57 +0900

    Update Runner.py
    다음 동작까지 걸리는 시간만큼 스탑 해놓고 동작하는 반복 함수를만듬 STOP 은 아직

--- api/Runner.py
+++ api/Runner.py
@@ -1,25 +1,41 @@
+import pyautogui as pg
+
+
 """
 참고사항: 받은 dictionary데이터는
 {<다음동작까지 걸리는 시간>:<다음 동작>}
 형태로 이루어져 있음
 """
+# 직접 생성
+my_dict = {3: 'Alice', 2: "yes" , 5 : 'female'}
 
-isStop = False;
+
+
+isStop = False
 
 def start(dictionary):
-    isStop = False;
+    isStop = False
     #dictionary에 들어있는 행동들을 반복
     while not isStop:
-        print("반복중"); #이 줄은 삭제
-    return True;
+        for i in dictionary.keys():
+            pg.sleep(i)
+            print(dictionary.get(i))
+           
+        return True
+
 
 def repeatStart(dictionary):
     isStop = False;
     #dictionary에 들어있는 행동들을 무한반복
     while not isStop:
-        print("반복중") #이 줄은 삭제
-    return True;
+        for i in dictionary.keys():
+            pg.sleep(i)
+            print(dictionary.get(i))
+           
+            pg.sleep(1)
+    return True
+
 
 def stop():
-    isStop = True;
-    return True;
+    isStop = True
+    return True
