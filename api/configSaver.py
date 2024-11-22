25c53f01b5254b779a53bb0136026373aa52ad8b
catking562 <taewookim562@gmail.com>
2024-11-22 09:56:47 +0900

    add configSaver.py

---
+++ api/configSaver.py
@@ -0,0 +1,13 @@
+#파일 이름은 option.txt로 저장함!!!
+
+def get(key):
+    print("{key}값에 해당하는 데이터를 가져옴");
+
+def put(key, value):
+    print("{key}값에 해당하는 데이터를 {value}로 설정함");
+
+def save():
+    print("저장된 모든 {key:value} 데이터를 저장함");
+
+def load():
+    print("저장된 모든 데이터들을 불러옴");
