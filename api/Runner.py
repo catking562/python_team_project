53e29823218afa7c1cd90d6c4806c6acbae5cd84
leekhred <163300190+leekhred@users.noreply.github.com>
2024-12-04 11:56:16 +0900

    Update Runner.py
    success

--- api/Runner.py
+++ api/Runner.py
@@ -1,57 +1,72 @@
 from pynput.mouse import Controller, Button
-from pynput.keyboard import Controller,Key
+from pynput.keyboard import Controller as KeyboardController, Key
 import time
 
-# Mouse Controller 초기화
+# 마우스와 키보드 컨트롤러 초기화
 mouse = Controller()
+keyboard = KeyboardController()
 
-my_tuple=((1,'click',Button.left,True),(1,'scroll',1,3,2,4),(2,'press0',Key.enter))
-isStop = False
-#tuple에 들어있는 행동들을 반복
+
+my_tuple = [
+    (1, 'click', Button.left, True),  
+    (1, 'scroll', 1, 3, 2, 4),       
+    (2, 'press0', Key.enter)          
+]
+
+isStop = False  
+
+#반복 시작
 def start(tup):
-    global isStop , i
+    global isStop  
     isStop = False
-    for i in my_tuple:
-        time.sleep(i[0])
-        case(i[1])
-
+    for i in tup:
+        time.sleep(i[0])  
+        type_case(i)  
         if isStop:
             return True
-        
 
-
+# 반복을 멈추는 함수
 def stop():
+    global isStop
     isStop = True
-    return isStop
 
-#start(my_tuple)
+# 행동 처리 함수
+def type_case(action):
+    if action[1] == "click":
+        type_press = action[1]
+        button = action[2]
+        pressed = action[3]
+        if pressed==True:
+            mouse.press(button)
+        else:
+            mouse.release(button)
+        #return type_press , button , pressed
 
+    elif action[1] == "scroll":
+        type_press = action[1]
+        x = action[2]
+        y = action[3]
+        dx = action[4]
+        dy = action[5]
+        mouse.scroll(dx, dy)
+        #return type_press , x, y, dx, dy
 
+    elif action[1] == "press0":
+        type_press = action[1]
+        key = action[2]
+        keyboard.press(key)
+        keyboard.release(key)
+        #return type_press, key
+
+# 반복적으로 실행하는 함수
 def repeatStart(tup):
-    isStop = False
-    while not isStop: #isstop이 참일 경우 while 루프를 벗어나도록 함
-      for i in range(0,len(tup)):
-            time.sleep(tup[i][0])
-            if isStop: #도중에 정지버튼을 누르면 정지하도록 함
+    global isStop  
+    while not isStop:
+        for i in tup:
+            time.sleep(i[0])  
+            type_case(i)  
+            if isStop:
                 return True
-    return True
 
+start(my_tuple)
 
-def case(n):
-    if n == "click":
-        type_press = i[1]
-        button = i[2]
-        pressed = i[3]
-        return type_press,button,pressed
-    
-    elif n == "scroll":
-        type_press = i[1]
-        x = i[2]
-        y= i[3]
-        dx = i[4]
-        dy = i[5]
-        return type_press , x, y , dx , dy
-    elif n == "press0":
-        type_press = i[1]
-        key = i[2]
-        return type_press , key
