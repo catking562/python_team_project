5046526056b230a3b16062740665ab75b0e565d0
tmin1d <wjddn6640@naver.com>
2024-12-05 19:44:16 +0900

    Update DataEncoder.py

--- api/DataEncoder.py
+++ api/DataEncoder.py
@@ -1,11 +1,5 @@
-def encoding(dictionary):
-    encode = {};
-    #dictionary데이터를 encode로 인코딩
-    #데이터를 {<다음 동작까지 걸리는 시간>:<다음 동작>}
-    #으로 변경
-    return encode;
 
-def encode(control):
+def encoding(control):
     control = sorted(control, key = lambda x:x[0])
     startTime = control[0][0]
 
