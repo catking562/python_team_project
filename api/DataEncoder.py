4b8956adabe743b8bb827d2a41650822d0bffffb
tmin1d <wjddn6640@naver.com>
2024-12-05 20:14:58 +0900

    Update DataEncoder.py

--- api/DataEncoder.py
+++ api/DataEncoder.py
@@ -1,12 +1,21 @@
 
-def encoding(control):
+def encoding_Torun(control):
     control = sorted(control, key = lambda x:x[0])
     startTime = control[0][0]
 
-    for i in control :
-        control[0][0] = control[0][0] - startTime #시작 시간으로부터 행동 시간을 뺀 간격.
+    for i in range(len(control)) :
+        control[i][0] = control[i][0] - startTime #시작 시간으로부터 행동 시간을 뺀 간격.
 
     return control;
 
-def encode_str(control):
+def encoding_Tostr(control):
+    for i in range(len(control)):
+        control[i] = ','.join(control[i])
+
+    control_str = '\n'.join(control)
     return control
+
+def encoding_bystr(save_string):
+    control = save_string.split('\n')
+    for i in range(len(control)):
+        len
