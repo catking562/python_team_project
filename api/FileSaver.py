32f2e9c0921252d386b9655924f918741f7d38ed
tmin1d <wjddn6640@naver.com>
2024-12-07 22:18:33 +0900

    Update FileSaver.py
    list 형태의 조작값을 문자열 형태로 전달됨.
    saveFile 함수를 전달된 문자열을 그대로 파일에 저장하는 형태로 바꿈.
    loadFile 함수를 파일 안에 있는 문자열을 읽고 전달하는 형태로 바꿈.

--- api/FileSaver.py
+++ api/FileSaver.py
@@ -1,6 +1,6 @@
 import os
 
-def saveFile(tup, fileName):
+def saveFile(control_str, fileName):
     """
     튜플 데이터를 지정된 파일에 저장합니다.
     (시간, 동작) 형태의 튜플을 저장
@@ -8,22 +8,8 @@ def saveFile(tup, fileName):
     fileloc = f"data/{fileName}.txt"
     # 파일을 쓰기 모드로 열기
     with open(fileloc, "w", encoding="utf-8") as file:
-        for item in tup:
-            # 시간과 동작을 저장
-            time = item[0]
-            action = item[1]
-            
-            # 각 동작에 따른 튜플 내용 저장
-            if action == "click":
-                x, y, button, pressed = item[2], item[3], item[4], item[5]
-                file.write(f"{time}:{action}:{x}:{y}:{button}:{pressed}\n")
-            elif action == "scroll":
-                x, y, dx, dy = item[2], item[3], item[4], item[5]
-                file.write(f"{time}:{action}:{x}:{y}:{dx}:{dy}\n")
-            elif action == "press" or action == "release":
-                key = item[2]
-                file.write(f"{time}:{action}:{key}\n")
-    
+        file.write(control)
+
     print(f"파일 저장 성공: {fileloc}")
     return True
 
@@ -33,27 +19,12 @@ def loadFile(fileName):
     (시간, 동작) 형태의 튜플을 불러옴
     """
     fileloc = f"data/{fileName}.txt"    
-    tup = []
     # 파일을 읽기 모드로 열기
     with open(fileloc, "r", encoding="utf-8") as file:
-        for line in file:
-            parts = line.strip().split(":")
-            time = float(parts[0])
-            action = parts[1]
-            
-            # 각 동작에 따라 튜플을 다르게 파싱
-            if action == "click":
-                x, y, button, pressed = int(parts[2]), int(parts[3]), parts[4], bool(int(parts[5]))
-                tup.append((time, action, x, y, button, pressed))
-            elif action == "scroll":
-                x, y, dx, dy = int(parts[2]), int(parts[3]), int(parts[4]), int(parts[5])
-                tup.append((time, action, x, y, dx, dy))
-            elif action == "press" or action == "release":
-                key = parts[2]
-                tup.append((time, action, key))
+        action_str = file.readlines()
     
     print(f"파일 로드 성공: {fileloc}")
-    return tup
+    return action_str
 
 
 # 지금까지 저장된 파일이름을 리스트로 반환
