30cfcedd6c94575df04ee804d5dc568c5fc9c60b
tmin1d <wjddn6640@naver.com>
2024-12-07 22:21:02 +0900

    Update DataEncoder.py

--- api/DataEncoder.py
+++ api/DataEncoder.py
@@ -11,22 +11,10 @@ def encoding_Torun(control):
     return control;
 
 def encoding_Tostr(control):
-    for i in range(len(control)):
-        control[i] = ','.join(control[i])
-
-    control_str = '\n'.join(control)
-    return control
+    control_str = repr(control)
+    return control_str
 
 def encoding_bystr(save_string):
-    control = save_string.split('\n')
-    
-    for i in range(len(control)):
-        control[i] = control[i].split(',')
-        
-    for i in range(len(control)):
-        for j in range(len(control[j])):
-            if (control[i][j].isdigit() == True):
-                control[i][j] = int(control[i][j])
-                
+    control = eval(save_string)
     return control
 
