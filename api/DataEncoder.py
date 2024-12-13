b541b79b37da570c6fa18b30b5bec9629dbf0d23
tmin1d <wjddn6640@naver.com>
2024-12-13 12:09:12 +0900

    Update DataEncoder.py

--- api/DataEncoder.py
+++ api/DataEncoder.py
@@ -1,24 +1,24 @@
 import pynput;
+from pynput.keyboard import Key
+
+
+# Key 클래스의 __repr__을 재정의
+def custom_repr(self):
+    return f"{self.value}"
+
+# Key 클래스의 __repr__ 메서드 변경
+Key.__repr__ = custom_repr
+
 
 #단축키 딕셔너리를 넣으면 문자열 형태로 변경됨.
 #주의 : 그대로 파일에 써넣기만 할 것
 def encode_save_hotkeys(hotkeys):
-    temp_dic = {}
-
-    for key, value in hotkeys.items():
-        if isinstance(value, str):
-            temp_dic[key] = value
-        else:
-            str_value = str(value)
-            temp_dic[key] = "pynput.keybord." + str_value
-    
-    str_hotkeys = repr(temp_dic)
+    str_hotkeys = repr(hotkeys)
     return str_hotkeys
 
 #단축키 문자열을 딕셔너리로 바꿈.
 #사용 방법 : hotkeys = encode_load_hotkeys(str_hotkeys) 형태로 사용
 def encode_load_hotkeys(str_hotkeys):
-    
     return eval(str_hotkeys)
     
 def encoding_Torun(control):
