cda5dc494456d35023fe9be6050d35692a9791b9
tmin1d <wjddn6640@naver.com>
2024-12-13 09:19:54 +0900

    Update DataEncoder.py

--- api/DataEncoder.py
+++ api/DataEncoder.py
@@ -22,14 +22,12 @@ def encode_load_hotkeys(str_hotkeys):
     return eval(str_hotkeys)
     
 def encoding_Torun(control):
-    startTime = control[0];
-    del control[0];
-    print(control[0][0]);
     control = sorted(control, key = lambda x:x[0])
-
-    for i in range(len(control)):
+    startTime = control[0][0]
+    
+    for i in range(len(control)) :
         control[i][0] = control[i][0] - startTime #시작 시간으로부터 행동 시간을 뺀 간격.
-        startTime+=control[i][0];
+
     return control;
 
 def encoding_Tostr(control):
