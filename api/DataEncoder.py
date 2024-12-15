5ca2f7cb13e702e8c7b5ef08c6abf211afcf4073
tmin1d <wjddn6640@naver.com>
2024-12-15 20:17:31 +0900

    Update DataEncoder.py

--- api/DataEncoder.py
+++ api/DataEncoder.py
@@ -1,26 +1,12 @@
 import pynput;
-from pynput.keyboard import Key
-
 
 # Key 클래스의 __repr__을 재정의
 def custom_repr(self):
-    return f"{self.value}"
+    return f"pynput.keyboard.{self}"
 
 # Key 클래스의 __repr__ 메서드 변경
-Key.__repr__ = custom_repr
+pynput.keyboard.Key.__repr__ = custom_repr
 
-
-#단축키 딕셔너리를 넣으면 문자열 형태로 변경됨.
-#주의 : 그대로 파일에 써넣기만 할 것
-def encode_save_hotkeys(hotkeys):
-    str_hotkeys = repr(hotkeys)
-    return str_hotkeys
-
-#단축키 문자열을 딕셔너리로 바꿈.
-#사용 방법 : hotkeys = encode_load_hotkeys(str_hotkeys) 형태로 사용
-def encode_load_hotkeys(str_hotkeys):
-    return eval(str_hotkeys)
-    
 def encoding_Torun(control):
     control = sorted(control, key = lambda x:x[0])
     startTime = control[0][0]
@@ -30,6 +16,10 @@ def encoding_Torun(control):
         startTime = startTime + control[i][0];
     return control;
 
+#파이썬 기본 자료형을 문자열로 변경
+#추가로 불러온 매소드는 따로 __repr__를 변경해야함.
+#pynput의 Key 클래스의 __repr__는 변경되었으므로 밑의 인코딩 함수만 사용하면 됨.
+
 def encoding_Tostr(control):
     control_str = repr(control)
     return control_str
@@ -37,4 +27,5 @@ def encoding_Tostr(control):
 def encoding_bystr(save_string):
     control = eval(save_string)
     return control
+
 
